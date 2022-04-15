c6a8b2f1db12ed7a496722b04c4d48ad3cf3d907
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-15 11:31:24 +0100

    adds player and game classes

---
+++ models/game.py
@@ -0,0 +1,25 @@
+class Game:
+    def __init__(self, player_one_choice, player_two_choice):
+        self.player_one_choice = player_one_choice
+        self.player_two_choice = player_two_choice
+        self._winner
+
+    def play_game(self, player_one, player_two):
+        if player_one.choice == "Rock":
+            if player_two.choice == "Scissors":
+                self._winner = player_one.name
+            else:
+                self._winner = player_two.name
+
+        if player_one.choice == "Scissors":
+            if player_two.choice == "Paper":
+                self._winner = player_one.name
+            else:
+                self._winner = player_two.name
+
+        if player_one.choice == "Paper":
+            if player_two.choice == "Rock":
+                self._winner = player_one.name
+            else:
+                self._winner = player_two.name
+        return self._winner
