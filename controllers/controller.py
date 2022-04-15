794b744ffd074998a22990213228a1f0cca540ba
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-15 15:15:02 +0100

    adds functionality to input form

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template
+from flask import redirect, render_template, request
 from app import app
 from models.game import Game
 from models.player import Player
@@ -9,10 +9,16 @@ def home():
 
 @app.route('/<player_one_choice>/<player_two_choice>', methods=['GET', 'POST'])
 def game_result(player_one_choice, player_two_choice):
-    player_one = Player("Player One", player_one_choice)
-    player_two = Player("Player Two", player_two_choice)
-    first_game = Game(player_one_choice, player_two_choice)
-    game_result = first_game.play_game(player_one, player_two)
+    player_one_name = request.form["name1"]
+    player_one_choice = request.form["choice1"]
+
+    player_two_name = request.form["name2"]
+    player_two_choice = request.form["choice2"]
+
+    player_one = Player(player_one_name, player_one_choice)
+    player_two = Player(player_two_name, player_two_choice)
+    game = Game(player_one_choice, player_two_choice)
+    game_result = game.play_game(player_one, player_two)
     
     return render_template("result.html", title="result", result=game_result)
     
