259c772884a517d1722db90c442b5996f6c0380c
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-15 11:48:25 +0100

    improves play game function

--- models/game.py
+++ models/game.py
@@ -3,23 +3,31 @@ class Game:
         self.player_one_choice = player_one_choice
         self.player_two_choice = player_two_choice
         self._winner
+        self._winner_choice
 
     def play_game(self, player_one, player_two):
         if player_one.choice == "Rock":
             if player_two.choice == "Scissors":
                 self._winner = player_one.name
+                self._winner_choice = player_one.choice
             else:
                 self._winner = player_two.name
+                self._winner_choice = player_two.choice
 
         if player_one.choice == "Scissors":
             if player_two.choice == "Paper":
                 self._winner = player_one.name
+                self._winner_choice = player_one.choice
             else:
                 self._winner = player_two.name
+                self._winner_choice = player_two.choice
 
         if player_one.choice == "Paper":
             if player_two.choice == "Rock":
                 self._winner = player_one.name
+                self._winner_choice = player_one.choice
             else:
                 self._winner = player_two.name
-        return self._winner
+                self._winner_choice = player_two.choice
+
+        return f'{self._winner} wins by playing {self._winner_choice}' 
