c2c13665c2de11c249a27f4a62fa6ff86fa46241
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-16 09:56:52 +0100

    changes game and player to use getters instead of accessing the attributes directly

--- models/game.py
+++ models/game.py
@@ -1,9 +1,9 @@
 class Game:
     def __init__(self, player_one, player_two):
-        self.player_one_name = player_one.name
-        self.player_one_choice = player_one.choice
-        self.player_two_name = player_two.name
-        self.player_two_choice = player_two.choice
+        self.player_one_name = player_one.get_name()
+        self.player_one_choice = player_one.get_choice()
+        self.player_two_name = player_two.get_name()
+        self.player_two_choice = player_two.get_choice()
         self._winner = ""
         self._winner_choice = ""
 
--- models/player.py
+++ models/player.py
@@ -2,3 +2,9 @@ class Player():
     def __init__(self, input_name, input_choice):
         self.name = input_name
         self.choice = input_choice
+    
+    def get_name(self):
+        return self.name
+    
+    def get_choice(self):
+        return self.choice
